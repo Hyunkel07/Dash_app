4ab18e11c4191ae9760bd610ae03cd95c26bf7a5
Hyunkel07 <58592723+Hyunkel07@users.noreply.github.com>
2025-01-30 16:51:18 +0100

    Update dash_generic_hdf5.py

--- dash_generic_hdf5.py
+++ dash_generic_hdf5.py
@@ -15,7 +15,7 @@ import numpy as np
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+server = app.server
 app.layout = html.Div([
     dcc.Upload(
         id='upload-data',
